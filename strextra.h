f4ed15daf491b7c53d380923fca298e21e1a3597
farjad1205 <farjad1205@gmail.com>
2025-08-31 23:27:14 -0500

    done

--- strextra.h
+++ strextra.h
@@ -4,13 +4,34 @@ using namespace std;
 
 int find(const string target, const string given)
 {
-    for (int i = 0; i < given.length() - target.length(); i++)
+    if (target.empty())
+        return 0;
+
+    for (int start = 0; start < given.length(); start++)
     {
-        if (target == given.substr(i, target.length()))
+        int tIndex = 0;
+        int gIndex = start;
+        int matchStart = -1;
+
+        while (tIndex < target.length() && gIndex < given.length())
         {
-            return i;
+            if (target[tIndex] == given[gIndex])
+            {
+                if (matchStart == -1)
+                {
+                    matchStart = gIndex;
+                }
+                tIndex++;
+            }
+            gIndex++;
+        }
+
+        if (tIndex == target.length())
+        {
+            return matchStart;
         }
     }
+
     return -1;
 }
 
